feed5ac6796ffce7ee134dff429e5dccc89e7fc8
riki <3603119045@qq.com>
2025-10-22 23:58:40 +0800

    Delete LinkME-backend directory

--- LinkME-backend/src/main/java/com/linkme/backend/module/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.linkme.backend.module.service;
-
-import com.linkme.backend.module.entity.User;
-
-public interface UserService {
-    User findById(Integer id);
-    User findByUsername(String username);
-    User register(User user, String rawPassword);
-}
